a7f79b3330ee9f77e1e215e23c96e74a88d95a61
alexis-gauluet <alexis.gauluet@edu.esiee.fr>
2024-11-15 13:26:09 +0000

    Exercice qui vérifie si une chaine de caractère est un palindrome

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-#### Fonction secondaire
+"""Code pour vérifier si une chaine de caractère est un palindrome,
+ c'est a dire se lit de la même manière de G à D et de D à G"""
+import re
+import unicodedata
 
 
 def ispalindrome(p):
-
+    """Verifie si une chaine de caracrère est un palindrome, renvoie vrai si c'est le cas"""
+    verif=True
     # votre code ici
-    
-    return False
+    sans_accent= ''.join(c for c in unicodedata.normalize('NFD', p)
+                  if unicodedata.category(c) != 'Mn')
+    sans_accent=sans_accent.lower().replace(" ","")
 
-#### Fonction principale
+    filtre= re.sub(r'[^a-zA-Z0-9]','',sans_accent)
+
+    for i in range (int(len(filtre)/2)) :
+        if filtre[i]!=filtre[len(filtre)-i-1] :
+            verif= False
+    return verif
+
+#### Fonction sans_accentrincisans_accentale
 
 
 def main():
+    """fonction executable"""
+    # vos asans_accentsans_accentels à la fonction secondaire ici
 
-    # vos appels à la fonction secondaire ici
-
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    for s in ["Radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
 
